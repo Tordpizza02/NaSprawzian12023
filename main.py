b754d18a2ed6142b327b38e6b7979d11573bfb17
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2023-10-18 11:16:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#1 mnożenie 
+# a = int(input())
+# b = int(input())
+# print(a*b)
+
+# #funkcja
+# def mnożenie(n, o):
+#   wynik = n*o
+#   return(wynik)
+# int f (n)
+# if (n==0) return 3
+# else return f(n-1)+2
