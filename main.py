0ec7d2afa1717cc912b8a768669cfeea8480a70e
Tordpizza02 <114918421+Tordpizza02@users.noreply.github.com>
2023-10-19 11:33:50 +0000

    tabliczka mnożenia

--- main.py
+++ main.py
@@ -7,6 +7,14 @@
 # def mnożenie(n, o):
 #   wynik = n*o
 #   return(wynik)
+
 # int f (n)
 # if (n==0) return 3
 # else return f(n-1)+2
+
+# tabliczka mnożenia
+# for i in range(1,11):
+#   for j in range(1,11):
+#     wynik = i*j
+#     print(f"{i} x {j} = {wynik}")
+
